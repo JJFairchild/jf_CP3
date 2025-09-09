a69560f37262cfc4e3f0caa701bd79d469ed04f7
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-09-09 11:50:43 -0600

    Wrong thing :(

--- Projects/Dynamic Array/main.cpp
+++ Projects/Dynamic Array/main.cpp
@@ -32,3 +32,37 @@ HOW TO SUBMIT:
 #include <iostream>
 #include <memory>
 using namespace std;
+
+int capacity = 5;
+unique_ptr<string[]> stuff(new string[capacity]);
+int entries = 0;
+
+void check_size(){
+    if (entries == capacity) {
+        capacity += 5;
+        unique_ptr<string[]> temp(new string[capacity]);
+        for (int i=0; i<entries; i++) {
+            temp[i] = stuff[i];
+        }
+        stuff = move(temp);
+    }
+}
+
+int main(){
+
+    while (true) {
+        string thing;
+        cout << "What media (book, comic, show, movie, etc.) do you want to add to your list? (type 'cancel' to quit): ";
+        getline(cin, thing);
+        if (thing == "cancel") break;
+        stuff[entries] = thing;
+        entries++;
+        check_size();
+    }
+
+    for (int i=0; i<entries; i++) {
+        cout << stuff[i] << endl;
+    }
+
+    return 0;
+}
