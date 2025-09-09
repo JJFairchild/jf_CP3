6b5ec766a7781543129ce7881dd5227c02630c1b
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-09-09 11:15:52 -0600

    File reorganization

---
+++ Projects/Dynamic Array/main.cpp
@@ -0,0 +1,34 @@
+// Jonas Fairchild, Dynamic Array
+
+/*
+
+Lesson 6: Pointers
+LESSON I CAN: I CAN use pointers in C++
+
+By the end of this lesson you will be able to:
+- Define pointers
+- Use pointers in a program
+- Describe the benefits of pointers
+
+State Standards:
+***This is a UVU CS1410 requirement***
+
+INSTRUCTIONS:
+- Write a program that asks the user to enter books, movies, shows, colors, comics, etc. until they can't think of anymore. 
+- Start your array with only 5 spaces. Continue to increase the size of the array as your user hits the array cap. 
+- THEN, print out each of the inputs they gave you!
+
+KEY REMINDERS:
+- Remember you have to set your variable types
+- You have to get the standard library to create your inputs and outputs
+- Make sure your arrows are pointing the right way for the input/output stream. 
+- You have to set the length of the array when you create it
+
+HOW TO SUBMIT:
+- Submit the link to your project on Github
+
+*/
+
+#include <iostream>
+#include <memory>
+using namespace std;
