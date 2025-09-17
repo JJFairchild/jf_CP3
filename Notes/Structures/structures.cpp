f8cba1128735201997b9e06afcf8b9bb4c6b9c37
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-09-17 11:23:52 -0600

    Finished Structures Notes

--- Notes/Structures/structures.cpp
+++ Notes/Structures/structures.cpp
@@ -14,19 +14,6 @@ struct Movie{
     string title;
     Date releaseDate;
     bool isPopular = true;
-
-    bool == (Movie movie){
-        if (title == movie.title &&
-            releaseDate.year == movie.releaseDate.year &&
-            releaseDate.month == movie.releaseDate.month &&
-            releaseDate.day == movie.releaseDate.day &&
-            isPopular == movie.isPopular
-        ){
-            return true;
-        } else {
-            return false;
-        }
-    }
 };
 
 struct Customer{
@@ -34,6 +21,29 @@ struct Customer{
     string name;
     string email;
 };
+
+bool operator==(const Movie& first, const Movie& second){
+        return (first.title == second.title &&
+            first.releaseDate.year == second.releaseDate.year &&
+            first.releaseDate.month == second.releaseDate.month &&
+            first.releaseDate.day == second.releaseDate.day &&
+            first.isPopular == second.isPopular
+        );
+    }
+
+ostream& operator<<(ostream& stream, Movie& movie){
+    stream << movie.title;
+    return stream;
+}
+
+ostream& operator<<(ostream& stream, Customer& customer){
+    stream << customer.name;
+    return stream;
+} 
+
+void showMovie(Movie* movie){
+    cout << movie -> title << endl;
+}
 
 int main(){
     vector<Movie> movies;
@@ -50,6 +60,7 @@ int main(){
 
     Movie movie1 = {"Terminator", {1984, 6, 1}};
     Movie movie2 = {"Terminator 2", 1987};
+    showMovie(&movie1);
 
     // auto [title, releaseYear, isPopular] {terminator};
     // cout << "Movie Title: " << terminator.title << endl;
@@ -62,7 +73,12 @@ int main(){
         cout << "Movie day: " << movie.releaseDate.day << endl;
         cout << "Movie popularity: " << movie.isPopular << endl;
     }
-    cout << movie1 == movie2 << endl;
+    if (movie1 == movie2){
+        cout << movie1 << " is equal" << endl;
+    } else {
+        cout << movie1 << " is not " << movie2 << endl;
+    }
+    cout << (movie1 == movie1) << endl;
 
     Customer john = {23998754, "John Clarke", "johnclarke@gmail.com"};
 
