ba34969dc545974cf3ce72b596da9d34b690ac26
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-10-01 11:21:46 -0600

    Finished Text Files Notes

--- Notes/Text Files/text_files.cpp
+++ Notes/Text Files/text_files.cpp
@@ -69,29 +69,28 @@ int main(){
     ofile.open("data.csv");
     if(ofile.is_open()){
         ofile <<
-        "id, title, year\n" <<
-        "1, Terminator, 1984\n" <<
-        "2, Terminator 2, 1991\n";
+        "id,title,year\n" <<
+        "1,Terminator,1984\n" <<
+        "2,Terminator 2,1991\n";
         ofile.close();
     }
+
     ifile.open("data.csv");
+    string line;
+    vector <Movie> movies;
     if(ifile.is_open()){
-        vector <Movie> movies;
-        string str;
-
-        getline(ifile, str);
-
-        while(!ifile.eof()){
-            getline(ifile, str, ',');
-            if(str.empty()) continue;
+        getline(ifile, line);
+        while(getline(ifile, line)){
+            istringstream iss(line);
+            string item;
             Movie movie;
-            movie.id = stoi(str);
 
-            getline(ifile, str, ',');
-            movie.title = str;
-
-            getline(ifile, str);
-            movie.year = stoi(str);
+            getline(iss, item, ',');
+            movie.id = stoi(item);
+            getline(iss, item, ',');
+            movie.title = item;
+            getline(iss, item, ',');
+            movie.year = stoi(item);
 
             movies.push_back(movie);
         }
