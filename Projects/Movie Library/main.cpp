396ef5f59549466df6a5080fdb18cefc1bde3ee2
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-10-06 11:48:01 -0600

    quite literally nothing :)

---
+++ Projects/Movie Library/main.cpp
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+- Create an application that manages your Movie Library using structures to store movie details and enumerations for menu options. Your program should persist movie data by reading from and writing to an external file in CSV format.
+- Main menu needs to allow our user to load the library from a file (I will use a different one than the example one I have given you), view all movies, add a movie, delete a movie, and search movies.
+- The search menu should allow the user to select what they would like to search by, then the specific value they would like to search. 
+
+NOTE: You need to build a sequential search algorithm for this project (it is a state standard) 
+
+EXAMPLE:
+- Rating (e.g., G, PG, PG-13, R)
+- Director (e.g., Christopher Nolan)
+- Release Year (e.g., 1987)
+- Genre (e.g., Action, Comedy)
+
+KEY REMINDERS:
+- Use structures to organize movie data.
+- Use enumerations for the menu.
+- Incorporate input validation and error handling.
+- Make sure the program is user-friendly and clear.
+
+HOW TO SUBMIT:
+- Submit the link to your project on GitHub.
+*/
