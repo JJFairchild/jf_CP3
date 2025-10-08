3112064463252dbb5a0ffe102586f51cbe2f6a7a
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-10-08 11:48:39 -0600

    Unfinished Movie Library

--- Projects/Movie Library/main.cpp
+++ Projects/Movie Library/main.cpp
@@ -1,3 +1,5 @@
+// Jonas Fairchild
+
 /*
 INSTRUCTIONS:
 - Create an application that manages your Movie Library using structures to store movie details and enumerations for menu options. Your program should persist movie data by reading from and writing to an external file in CSV format.
@@ -21,3 +23,139 @@ KEY REMINDERS:
 HOW TO SUBMIT:
 - Submit the link to your project on GitHub.
 */
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+using namespace std;
+
+struct Movie{
+    string title;
+    string dir;
+    int year;
+    string genre;
+    string rating;
+};
+
+string getLib(){
+
+    string filename;
+    cout << "Input your library's file name: ";
+    getline(cin, filename);
+    ifstream file(filename);
+
+    if (file) {
+        cout << "Successfully added library." << endl;
+        return filename;
+    } else {
+        cout << "Sorry, couldn't find that file.\n";
+        return "";
+    }
+}
+
+vector<Movie> readLib(string filename){
+    vector <Movie> movies;
+
+    try{
+        ifstream file(filename);
+
+        string line;
+
+        if (file.is_open()){
+
+            getline(file, line);
+
+            while (getline(file, line)){
+                istringstream iss(line);
+                string item;
+                Movie mov;
+
+                getline(iss, item, ',');
+                mov.title = item;
+                getline(iss, item, ',');
+                mov.dir = item;
+                getline(iss, item, ',');
+                mov.year = stoi(item);
+                getline(iss, item, ',');
+                mov.genre = item;
+                getline(iss, item, ',');
+                mov.rating = item;
+
+                movies.push_back(mov);
+            }
+
+            file.close();
+        }
+    } catch (...){
+        cout << "Unable to read the given library. Ensure it is a csv and follows the proper format (title, director, year, genre, rating).\n";
+    }
+    return movies;
+}
+
+void displayMov(vector<Movie> movies){
+    for(auto& movie : movies){
+        cout << 
+        "- " << movie.title <<
+        "\n\t- " << ;
+    }
+}
+
+vector<string> seqSearch(vector<string> movies, string search){
+
+}
+
+enum MainMenu{
+    SelectLib = 1,
+    ViewMov,
+    DelMov,
+    SearchMov,
+    Exit
+};
+
+int main(){
+
+    string library;
+    vector<Movie> movies;
+
+    while(true){
+        string line;
+        cout <<
+        "\nWhat do you want to do?\n" <<
+        "1: Select a movie library\n" <<
+        "2: View the movie list\n" <<
+        "3: Delete a movie\n" <<
+        "4: Search the movie list\n" <<
+        "5: Save and exit\n";
+        getline(cin, line);
+
+        try{
+            int choice = stoi(line);
+
+            if (choice = MainMenu::SelectLib){
+                string lib = getLib();
+                if (!lib.empty()){
+                    library = lib;
+
+                    movies = readLib(library);
+                }
+            } else if (choice = MainMenu::ViewMov){
+
+            } else if (choice = MainMenu::DelMov){
+
+            } else if (choice = MainMenu::SearchMov){
+
+            } else if (choice = MainMenu::Exit){
+                break;
+            } else {
+                cout << "That's not an option. Try again.\n";
+            }
+        } catch(...){
+            cout << "Invalid input. Try again.\n";
+        }
+    }
+
+    return 0;
+}
