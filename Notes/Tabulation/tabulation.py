77f4efb760d10a91799191e2279106bc9f7bd1c1
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-10-29 11:21:27 -0600

    Larose

--- Notes/Tabulation/tabulation.py
+++ Notes/Tabulation/tabulation.py
@@ -1,13 +1,12 @@
 # Jonas Fairchild, Tabulation
 
-import tabulate
+from tabulate import tabulate
 
 data = [
-    ["Bruce Wayne", "Batman", 6535907546], 
-    ["Oliver Queen", "Green Arrow", 5873222983], 
-    ["Clark Kent", "Reporter", 8743298754],
-    ["Barry Allen", "CSI", 1283272345],
-    ["John Smith", "Unemployment", 4582054231]
+    ["Will", 15],
+    ["Hope", 19],
+    ["Jill", 20],
 ]
 
-print(tabulate(data, headers=["Name", "Job", "Number"]))
+
+print(tabulate(data, headers=["Character", "Age"], tablefmt="fsql"))
--- Projects/Dessert Shop/dessert.py
+++ Projects/Dessert Shop/dessert.py
@@ -3,19 +3,23 @@ from abc import ABC, abstractmethod
 class DessertItem(ABC):
     def __init__(self, name="", tax_percent=7.25):
         self.name = name
+        self.tax_percent = tax_percent
     
+    def calculate_tax(self):
+        return round(self.tax_percent / 100 * self.calculate_cost(), 2)
+
     @abstractmethod
     def calculate_cost(self):
         pass
-
-    def calculate_tax(self):
-        return self.tax_percent * self.calculate_cost()
 
 class Candy(DessertItem):
     def __init__(self, name="", weight=0.0, priceppound=0.0):
         super().__init__(name)
         self.weight = weight
         self.priceppound = priceppound
+    
+    def calculate_cost(self):
+        return round(self.priceppound * self.weight, 2)
         
 class Cookie(DessertItem):
     def __init__(self, name="", ccount=0, pricepdozen=0.0):
@@ -23,17 +27,26 @@ class Cookie(DessertItem):
         self.ccount = ccount
         self.pricepdozen = pricepdozen
         
+    def calculate_cost(self):
+        return round(self.pricepdozen * (self.ccount / 12), 2)
+    
 class IceCream(DessertItem):
     def __init__(self, name="", scount=0, pricepscoop=0.0):
         super().__init__(name)
         self.scount = scount
         self.pricepscoop = pricepscoop
     
+    def calculate_cost(self):
+        return round(self.pricepscoop * self.scount, 2)
+
 class Sundae(IceCream):
     def __init__(self, name="", scount=0, pricepscoop=0.0, tname="", tprice=0.0):
         super().__init__(name, scount, pricepscoop)
         self.tname=tname
         self.tprice=tprice
+
+    def calculate_cost(self):
+        return round(self.pricepscoop * self.scount + self.tprice, 2)
 
 class Order:
     def __init__(self, orders=[]):
@@ -44,3 +57,15 @@ class Order:
 
     def __len__(self):
         return len(self.orders)
+    
+    def order_cost(self):
+        cost = 0.0
+        for order in self.orders:
+            cost += order.calculate_cost()
+        return cost
+    
+    def order_tax(self):
+        tax = 0.0
+        for order in self.orders:
+            tax += order.calculate_tax()
+        return tax
