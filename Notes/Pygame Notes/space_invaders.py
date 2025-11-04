3ab38354b486b43966e77b182b086f2646176dc2
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-11-04 11:49:52 -0700

    MAKE SURE TO STOP THE GAME WHEN YOU LOSE

--- Notes/Pygame Notes/space_invaders.py
+++ Notes/Pygame Notes/space_invaders.py
@@ -3,31 +3,46 @@
 import pygame
 import random
 import math
-
+from pygame import mixer
 # Pygame setup
 pygame.init()
+
+
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Space Invaders")
-pygame_icon = pygame.image.load("Notes/Pygame Notes/Resources/ufo.png")
-pygame.display.set_icon(pygame_icon)
+icon = pygame.image.load("Notes/Pygame Notes/Resources/ufo.png")
+pygame.display.set_icon(icon)
+
+#bg = pygame.image.load("Notes/Pygame Notes/Resources/background.jpg")
+#bg = pygame.transform.scale(bg, (800,600))
+sfont = pygame.font.Font("freesansbold.ttf", 32)
+lfont = pygame.font.Font("freesansbold.ttf", 64)
+
+mixer.music.load("Notes/Pygame Notes/Resources/background.wav")
+mixer.music.play(-1)
+
+laser = mixer.Sound("Notes/Pygame Notes/Resources/laser.wav")
+explosion = mixer.Sound("Notes/Pygame Notes/Resources/explosion.wav")
 
 class Bullet:
     def __init__(self, x):
         self.x = x + 16
         self.y = 500
-        self.speed = -.2
+        self.speed = -.4
         self.state = "r"
         self.img = pygame.image.load("Notes/Pygame Notes/Resources/bullet.png")
-        self.rimg = pygame.transform.rotate(self.img, 90)
+        self.img = pygame.transform.rotate(self.img, 90)
 
     def show(self):
-        screen.blit(self.rimg, (self.x, self.y))
+        if self.state == "f":
+            screen.blit(self.img, (self.x, self.y))
         
 
     def move(self):
-        self.y += self.speed
-        if self.y <= -32:
-            self.state = "r"
+        if self.state == "f":
+            self.y += self.speed
+            if self.y <= -32:
+                self.state = "r"
 
 
 class Player:
@@ -36,6 +51,7 @@ class Player:
         self.x = 368
         self.y = 500
         self.speed = speed
+        self.score = 0
         
 
     def show(self):
@@ -69,16 +85,35 @@ class Enemy:
             self.y += self.speedy
 
     def isHit(self, bullet):
-        if math.sqrt((self.x - bullet.x) ^ 2 + (self.y - bullet.y) ^ 2) < 27:
+        if math.sqrt((self.x - bullet.x) ** 2 + (self.y - bullet.y) ** 2) < 48 and bullet.state == "f":
+            return True
+        return False
+    
+    def lose(self):
+        if self.y > 500:
             return True
         return False
 
+
 player = Player()
-enemy = Enemy(
-    random.randint(0, 736), 
-    random.randint(0, 236)
-)
+enemies = []
+for _ in range(6):
+    enemies.append(Enemy(
+        random.randint(0, 736), 
+        random.randint(0, 236)
+    ))
 bullet = Bullet(player.x)
+
+def restart(enemies):
+    # If the player kills all the enemies, this spawns a new wave
+    if enemies == []:
+        for _ in range(6):
+            enemies.append(Enemy(
+                random.randint(0, 736), 
+                random.randint(0, 236)
+            ))
+    return enemies
+
 
 running = True
 while running:
@@ -99,22 +134,42 @@ while running:
                 player.speed = .2
             if keys[pygame.K_UP]:
                 if bullet.state == "r":
+                    laser.play()
                     bullet = Bullet(player.x)
                     bullet.state = "f"
         
         if event.type == pygame.KEYUP:
             if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
                 player.speed = 0
-            
+
+    for i, enemy in enumerate(enemies):
+        if enemy.isHit(bullet):
+            explosion.play()
+            player.score += 1
+            bullet.state = "r"
+            bullet.y = 500
+            del enemies[i]
+
+    enemies = restart(enemies)
+
+    # Move things
     player.move()
-    enemy.move()
+    for enemy in enemies:
+        enemy.move()
+        if enemy.lose():
+            enemies = []
+            lose = lfont.render("GAME OVER", True, (255, 255, 255))
+            screen.blit(lose, (200, 150))
+    bullet.move()
 
     # Display things
     screen.fill((0,0,0))
+    #screen.blit(bg)
+    score = sfont.render(f"Score: {player.score}", True, (255, 255, 255))
+    screen.blit(score, (10,10))
     player.show()
-    if bullet.state == "f":
-        bullet.move()
-        bullet.show()
-    enemy.show()
-
+    for enemy in enemies:
+        enemy.show()
+    bullet.show()
+    
     pygame.display.flip() # Refresh display
